554d104db5393ac834d8f9b1bd746608cc04234c
Matias <csmatiperreng@gmail.com>
2023-07-09 14:59:48 -0300

    Commit

--- src/componentes/ItemListContainer/index.jsx
+++ src/componentes/ItemListContainer/index.jsx
@@ -3,13 +3,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function ListContainer(props) {
   return (
-    <div className="container-fluid bg-light mt-4 p-4 rounded">
+    <div className="container-fluid rounded">
       <div className="greetings">
-        <h1 style={{ color: 'black', fontSize: '24px', textAlign: 'center'  }}>{props.greetings}</h1>
+        <h1 style={{ color: 'black', fontSize: '24px', textAlign: 'center', marginTop: '20px'  }}>{props.greetings}</h1>
       </div>
       <div className="row">
         {props.children.map((child, index) => (
-          <div key={index} className="col-lg-4 col-md-6">
+          <div key={index} className="col-lg-3 col-md-6">
             {child}
           </div>
         ))}
