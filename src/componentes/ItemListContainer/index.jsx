ba9729a94a133ca27f88818784ef495273ff1237
Matias <csmatiperreng@gmail.com>
2023-07-09 15:17:49 -0300

    Commit

--- src/componentes/ItemListContainer/index.jsx
+++ src/componentes/ItemListContainer/index.jsx
@@ -5,11 +5,11 @@ function ListContainer(props) {
   return (
     <div className="container-fluid rounded">
       <div className="greetings">
-        <h1 style={{ color: 'black', fontSize: '24px', textAlign: 'center', marginTop: '20px'  }}>{props.greetings}</h1>
+        <h1 style={{ color: 'black', fontSize: '24px', textAlign: 'center', marginTop: '25px'  }}>{props.greetings}</h1>
       </div>
       <div className="row">
         {props.children.map((child, index) => (
-          <div key={index} className="col-lg-3 col-md-6">
+          <div key={index} className="col-lg-4 col-md-6">
             {child}
           </div>
         ))}
