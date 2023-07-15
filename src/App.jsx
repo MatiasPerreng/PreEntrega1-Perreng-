f7dc4699759151b9a1d032e45e7ec908fcc0c5c6
Matias <csmatiperreng@gmail.com>
2023-07-15 13:27:37 -0300

    Cambio a Imagenes mayus

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import ListContainer from './componentes/ItemListContainer';
 import ItemContainer from './componentes/itemsOfContainer';
 import 'bootstrap/dist/css/bootstrap.css';
 import reactLogo from './assets/react.svg';
-import banana from './imagenes/Banana.webp'
-import manzana from './imagenes/Manzana.webp'
-import naranja from './imagenes/Naranja.webp'
-import mandarina from './imagenes/Mandarina.jpg'
-import tomate from './imagenes/Tomate.webp'
-import uvas from './imagenes/Uvas.jpg'
+import banana from './Imagenes/Banana.webp'
+import manzana from './Imagenes/Manzana.webp'
+import naranja from './Imagenes/Naranja.webp'
+import mandarina from './Imagenes/Mandarina.jpg'
+import tomate from './Imagenes/Tomate.webp'
+import uvas from './Imagenes/Uvas.jpg'
 
 function App() {
   return (
