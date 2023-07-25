43b5381dd3d0fdd938030d73496ddeca500f45eb
Matias <csmatiperreng@gmail.com>
2023-07-24 22:13:07 -0300

    Commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import NavBar from '../../componentes/Navbar';
 import ListItem from '../../componentes/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.css';
-import reactLogo from '../../assets/react.svg';
+
 
 const Home = () => {
-  const categories = ['Human', 'Alien', 'All'];
   const [characters, setCharacters] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
 
@@ -20,18 +18,9 @@ const Home = () => {
       });
   }, []);
 
-  const handleCategoryChange = (category) => {
-    setSelectedCategory(category === 'All' ? null : category);
-  };
 
   return (
     <>
-      <NavBar
-        child1={<img src={reactLogo} alt="React Logo" />}
-        data={categories}
-        onCategoryChange={handleCategoryChange}
-      />
-
       <ListItem
         greetings={'Bienvenidos a mi segunda entrega Coders'}
         data={characters}
