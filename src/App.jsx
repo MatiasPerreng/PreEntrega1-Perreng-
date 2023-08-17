43c4774e541298301fba96867c10b17d3cd08ea4
Matias <csmatiperreng@gmail.com>
2023-08-16 21:33:25 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
-
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc } from 'firebase/firestore'
 import Home from './pages/Home';
 import NavBarContainer from './componentes/Navbar';
 import Detail from './pages/Detail';
-import { useEffect } from 'react';
 import { CartProvider } from './context/CartContext';
 import Cart from './componentes/Cart/Cart';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAcFS0sYuXKBy7dI8fr95-DdmD1-d1Wcx4",
-  authDomain: "miproyectoperreng.firebaseapp.com",
-  projectId: "miproyectoperreng",
-  storageBucket: "miproyectoperreng.appspot.com",
-  messagingSenderId: "597161764110",
-  appId: "1:597161764110:web:be571a411e215f20b1235f",
-  measurementId: "G-XKS7Z9V1DY"
-};
-
-initializeApp(firebaseConfig);
-
-
-
 const App = () => {
-
-  useEffect(() => {
-    const db = getFirestore();
-    const products = doc(db, 'products', 'Zw2AgtWCeQTLtfkLWieV');
-    getDoc(products)
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          console.log('snapshot: ', snapshot.data())
-        }
-      })
-      .catch((error) => {
-        console.log('error: ', error)
-      })
-
-  }, [])
 
   return (
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,102 +2,40 @@ import React, { useEffect, useState } from 'react';
 import ListItem from '../../componentes/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useParams } from 'react-router-dom';
-import Rick from '../../images/Rick.jpeg';
-import Morty from '../../images/Morty.jpeg';
-import Summer from '../../images/Summer.jpeg';
-import Beth from '../../images/Beth.jpeg';
-import Jerry from '../../images/Jerry.jpeg';
-import Birdperson from '../../images/Birdperson.jpeg';
-import Squanchy from '../../images/Squanchy.jpeg';
+import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCO7Hsvasj7waNBATAbjFTTE3QpQlkQ4Nw",
+  authDomain: "proyectofinal-e6a17.firebaseapp.com",
+  projectId: "proyectofinal-e6a17",
+  storageBucket: "proyectofinal-e6a17.appspot.com",
+  messagingSenderId: "66903800873",
+  appId: "1:66903800873:web:8c53dee2d451b2c35ab038",
+  measurementId: "G-9JGYJRZFY8"
+};
+initializeApp(firebaseConfig);
 
 const Home = () => {
   const [characters, setCharacters] = useState([]);
   const { category } = useParams();
 
   useEffect(() => {
-    const fetchData = () => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          const charactersData = [
-            {
-              id: 1,
-              name: 'Rick Sanchez',
-              status: 'Alive',
-              species: 'Human',
-              gender: 'Male',
-              price: '410',
-              image: Rick,
-            },
-            {
-              id: 2,
-              name: 'Morty Smith',
-              status: 'Alive',
-              species: 'Human',
-              gender: 'Male',
-              price: '450',
-              image: Morty,
-            },
-            {
-              id: 3,
-              name: 'Summer Smith',
-              status: 'Alive',
-              species: 'Human',
-              gender: 'Female',
-              price: '220',
-              image: Summer,
-            },
-            {
-              id: 4,
-              name: 'Beth Smith',
-              status: 'Alive',
-              species: 'Human',
-              gender: 'Female',
-              price: '720',
-              image: Beth,
-            },
-            {
-              id: 5,
-              name: 'Jerry Smith',
-              status: 'Alive',
-              species: 'Human',
-              gender: 'Male',
-              price: '220',
-              image: Jerry,
-            },
-            {
-              id: 6,
-              name: 'Birdperson',
-              status: 'Alive',
-              species: 'Alien',
-              gender: 'Male',
-              price: '620',
-              image: Birdperson,
-            },
-            {
-              id: 7,
-              name: 'Squanchy',
-              status: 'Alive',
-              species: 'Alien',
-              gender: 'Male',
-              price: '120',
-              image: Squanchy,
-            },
-          ];
-          resolve(charactersData);
-        },);
-      });
-    };
+    const db = getFirestore();
+    const charactersCollection = collection(db, 'productos');
 
-    fetchData()
-      .then((data) => {
-        if (category) {
-          const filteredCharacters = data.filter(
-            (personaje) => personaje.species === category
-          );
-          setCharacters(filteredCharacters);
-        } else {
-          setCharacters(data);
-        }
+    let charactersQuery;
+    if (category) {
+      charactersQuery = query(charactersCollection, where('species', '==', category));
+    } else {
+      charactersQuery = charactersCollection;
+    }
+
+    getDocs(charactersQuery)
+      .then((querySnapshot) => {
+        const charactersData = querySnapshot.docs.map((doc) => doc.data());
+        setCharacters(charactersData);
       })
       .catch((error) => {
         console.log("Hubo un error: ", error);
