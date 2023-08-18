182646c6340f59fd106b2aa145df482b96a75e7f
Matias <csmatiperreng@gmail.com>
2023-08-18 19:06:05 -0300

    Commit

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,105 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import ItemDetailContainer from '../../componentes/itemDetailContainer';
 import { RotateLoader } from 'react-spinners';
 import { useParams } from 'react-router-dom';
-import Rick from '../../images/Rick.jpeg';
-import Morty from '../../images/Morty.jpeg';
-import Summer from '../../images/Summer.jpeg';
-import Beth from '../../images/Beth.jpeg';
-import Jerry from '../../images/Jerry.jpeg';
-import Birdperson from '../../images/Birdperson.jpeg';
-import Squanchy from '../../images/Squanchy.jpeg';
+import { FirestoreContext } from '../../context/FirestoreContext';
 
 const Detail = () => {
   const { id } = useParams();
+  const { characters } = useContext(FirestoreContext);
   const [character, setCharacter] = useState(null);
 
   useEffect(() => {
-    const charactersData = [
-      {
-        id: 1,
-        name: 'Rick Sanchez',
-        status: 'Alive',
-        species: 'Human',
-        gender: 'Male',
-        price: '410',
-        image: Rick,
-      },
-      {
-        id: 2,
-        name: 'Morty Smith',
-        status: 'Alive',
-        species: 'Human',
-        gender: 'Male',
-        price: '450',
-        image: Morty,
-      },
-      {
-        id: 3,
-        name: 'Summer Smith',
-        status: 'Alive',
-        species: 'Human',
-        gender: 'Female',
-        price: '220',
-        image: Summer,
-      },
-      {
-        id: 4,
-        name: 'Beth Smith',
-        status: 'Alive',
-        species: 'Human',
-        gender: 'Female',
-        price: '720',
-        image: Beth,
-      },
-      {
-        id: 5,
-        name: 'Jerry Smith',
-        status: 'Alive',
-        species: 'Human',
-        gender: 'Male',
-        price: '220',
-        image: Jerry,
-      },
-      {
-        id: 6,
-        name: 'Birdperson',
-        status: 'Alive',
-        species: 'Alien',
-        gender: 'Male',
-        price: '620',
-        image: Birdperson,
-      },
-      {
-        id: 7,
-        name: 'Squanchy',
-        status: 'Alive',
-        species: 'Alien',
-        gender: 'Male',
-        price: '120',
-        image: Squanchy,
-      },
-    ];
-
-    const characterData = charactersData.find(
+    const characterData = characters.find(
       (char) => char.id === parseInt(id)
     );
 
     setTimeout(() => {
       setCharacter(characterData);
     }, 1000);
-  }, []);
+  }, [characters, id]);
 
   if (!character) {
-    return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <RotateLoader
-        strokeColor="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        width="96"
-        visible={true}
-      />
-    </div>;
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <RotateLoader
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="96"
+          visible={true}
+        />
+      </div>
+    );
   }
 
   return (
